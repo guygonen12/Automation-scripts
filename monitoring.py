5c00ad13244bd2ce64bb6db5b5da6e3ee0f7546f
guygonen12 <guygonen12@gmail.com>
2025-02-22 19:00:53 +0200

    fixes

--- monitoring.py
+++ monitoring.py
@@ -1,15 +1,27 @@
 import psutil
+import re
 
 class active_connections:
     def __init__(self):
-        self.connections = show_all_connections()
+        self.connections = self.show_all_connections()
 
-    def show_all_connections():
+    def show_all_connections(self):
         connections = psutil.net_connections()
         all_connection = []
         for con in connections : 
             local_adress = f"{con.laddr.ip}:{con.laddr.port}" if con.laddr else "N/A"
-            remote_adress = f"{con.raddr.ip}:{con.raddr.ip}" if con.raddr else "N/A"
+            remote_adress = f"{con.raddr.ip}:{con.raddr.port}" if con.raddr else "N/A"
             status = con.status
             pid = con.pid #the process that opened the socket
             all_connection.append(f"local adress : {local_adress} | remote adress : {remote_adress} | status : {status} | PID : {pid}")
+        
+        return all_connection
+
+    def show_active_connections(connections):
+        active_connections = []
+        for con in connections : 
+            if re.search("ESTABLISHED",con):
+                active_connections.append(con)
+        return active_connections
+
+    
